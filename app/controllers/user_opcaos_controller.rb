20a8b4298d56d1a9116374892af021dad755d233
guilherme fontana <guifl2001@hotmail.com>
2019-09-02 17:46:25 -0300

    edit

--- app/controllers/user_opcaos_controller.rb
+++ app/controllers/user_opcaos_controller.rb
@@ -1,11 +1,11 @@
 class UserOpcaosController < ApplicationController
   def new
-    @user_opcao = User_opcao.new
+    @user_opcao = UserOpcao.new
   end
 
   def create
     @opcao = Opcao.find(params[:opcao_id])
-    @user_opcao = User_opcao.new
+    @user_opcao = UserOpcao.new
     @user_opcao.opcao = @opcao
     @user_opcao.user = current_user
     @user_opcao.save
