45cbc635010f08727d1ca5642a1d01af46d48ba2
guilherme fontana <guifl2001@hotmail.com>
2019-09-02 14:34:51 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: :show
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #faculdade
+  resources :faculdades, only: %i[show index]
+
+  #curso
+  resources :cursos, only: %i[show index]
+
+  #opcao
+  resources :opcaos, only: %i[show index]
+
+  #user_opcao
+  resources :user_opcaos, only: %i[create new delete]
 end
