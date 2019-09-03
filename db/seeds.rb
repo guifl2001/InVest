0db73ac2e73e50e36d6257d2a0ce484201a915c8
Guillaume Zeldine <g.zeldine@hotmail.fr>
2019-09-03 12:22:25 -0300

    Updated Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,82 +1,58 @@
-# puts "Creating Faculdades"
+puts "Deleting all"
+Opcao.delete_all
+Faculdade.delete_all
+Curso.delete_all
 
-# Faculdade.create(name: "USP")
-# Faculdade.create(name: "UNICAMP")
-# Faculdade.create(name: "INSPER")
-# Faculdade.create(name: "UNIFESP")
-
-# puts "Faculdades created"
-
-
-# puts "Creating Cursos"
-
-# p1 = Curso.create(name: "Administração")
-# p2 =Curso.create(name: "Engenharia de Computação")
-# p3 = Curso.create(name: "Arquitetura")
-# p4 = Curso.create(name: "Direito")
-# p5 = Curso.create(name: "Nutrição")
-
-# puts "Cursos Created"
-
-# puts "Creating Opcaos"
-
-# p1 = Opcao.create(faculdade_id: "1", curso_id: "1", data_inscricao: "07/03/2001", data_prova: "01/04/2001", ranking: 5)
-# p2 = Opcao.create(faculdade_id: "2", curso_id: "2", data_inscricao: "07/03/2001", data_prova: "01/04/2001", ranking: 4)
-# p3 = Opcao.create(faculdade_id: "3", curso_id: "3", data_inscricao: "07/03/2001", data_prova: "01/04/2001", ranking: 5)
-# p4 = Opcao.create(faculdade_id: "4", curso_id: "4", data_inscricao: "07/03/2001", data_prova: "01/04/2001", ranking: 3)
-# p5 = Opcao.create(faculdade_id: "1", curso_id: "5", data_inscricao: "07/03/2001", data_prova: "01/04/2001", ranking: 5)
-
-# puts "Opcaos Created"
-
+puts "Creating all"
 require 'csv'
 
 path = "/home/guillaume/code/guifl2001/InVest/db/universidade_cursos_updated.csv"
 
 # Seed para cadastrar todas as Faculdade sem repetidas
-# CSV.foreach(path) do |row|
-#     faculdade_existante = Faculdade.where(name: row[1])
-#     faculdade = Faculdade.new
-#     if faculdade_existante.empty? == false
-#       p "qaui"
-#     else
-#       p faculdade.name = row[1]
-#       p faculdade.sigla = row[2]
-#       faculdade.save
-#     end
-#   end
+CSV.foreach(path) do |row|
+    faculdade_existante = Faculdade.where(name: row[1])
+    faculdade = Faculdade.new
+    if faculdade_existante.empty? == false
+      "qaui"
+    else
+      p faculdade.name = row[1]
+      p faculdade.sigla = row[2]
+      faculdade.save
+    end
+  end
 
 # Seed para cadastrar todos os Cursos
-# CSV.foreach(path) do |row|
-#   curso = Curso.new
-#   curso_existante = Curso.where(name: row[4])
-#   if curso_existante.empty? == false
-#     p "qsd"
-#   else
-#     p curso.name = row[4]
-#     p curso.OCDE_geral = row[11]
-#     curso.save
-#   end
-# end
+CSV.foreach(path) do |row|
+  curso = Curso.new
+  curso_existante = Curso.where(name: row[4])
+  if curso_existante.empty? == false
+    "qsd"
+  else
+    p curso.name = row[4]
+    p curso.OCDE_geral = row[11]
+    curso.save
+  end
+end
 
 CSV.foreach(path) do |row|
   opcao = Opcao.new
   faculdade = Faculdade.where(name: row[1])
-  p faculdade
+  faculdade
   curso = Curso.where(name: row[4])
-  p curso
+  curso
   opcao_existante = Opcao.where(faculdade: faculdade).where(curso: curso)
 
   if opcao_existante.empty? == false
-    p "sdf"
+    "sdf"
   else
-    opcao.faculdade = faculdade[0]
-    opcao.curso = curso[0]
-    opcao.grau = row[5]
-    opcao.modalidade = row[6]
-    opcao.ano_criacao = row[10]
-    opcao.CC = row[7]
-    opcao.vagas = row[9]
-    opcao.ENADE = row[8]
+    p opcao.faculdade = faculdade[0]
+    p opcao.curso = curso[0]
+    p opcao.grau = row[5]
+    p opcao.modalidade = row[6]
+    p opcao.ano_criacao = row[10]
+    p opcao.CC = row[7]
+    p opcao.vagas = row[9]
+    p opcao.ENADE = row[8]
     p opcao.save
   end
 end
