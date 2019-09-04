f4c8f74b6da4b525f81ec6b3518a9e8a5b4c934f
Jose Eduardo Azevedo <eduardoazevedo08@gmail.com>
2019-09-04 17:20:03 -0300

    new falculdades page 1.2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,10 @@
 import "bootstrap";
+
+
+
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
