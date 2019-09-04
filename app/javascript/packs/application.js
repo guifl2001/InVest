7b75ebb07d47ecdb9f0f3ed6148c4714e9062d07
Bruno Tostes <btfjulio@hotmail.com>
2019-09-04 14:18:40 -0300

    design home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 import 'mapbox-gl/dist/mapbox-gl.css';
-
+import AOS from 'aos';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
+AOS.init();
