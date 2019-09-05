9e204378829b90b3a826ea309d89d5f390b42edb
Guillaume Zeldine <51140364+LovlsGenesis@users.noreply.github.com>
2019-09-05 16:15:44 -0300

    Revert "new faculdades page 1.3"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,18 @@
 import "bootstrap";
 
 
-import { loadDynamicBannerText } from '../components/banner';
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import AOS from 'aos';
+import { initMapbox } from '../plugins/init_mapbox';
+import { loadDynamicBannerText } from '../components/banner';
+
 initUpdateNavbarOnScroll();
+if (window.location.pathname === '/') { loadDynamicBannerText() };
+initMapbox();
 
 import { table } from '../components/table';
 table();
 
-import AOS from 'aos';
 AOS.init();
-
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { initMapbox } from '../plugins/init_mapbox';
-if (window.location.pathname === '/') { loadDynamicBannerText() };
-initMapbox();
-
-
-
-
