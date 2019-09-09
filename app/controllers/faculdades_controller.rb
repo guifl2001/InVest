ddbadfdc30fa8936fa1cd6e037acfdf6f403fd47
Guillaume Zeldine <g.zeldine@hotmail.fr>
2019-09-09 10:11:58 -0300

    Search on all index,  cursos / opçaos / faculdades

--- app/controllers/faculdades_controller.rb
+++ app/controllers/faculdades_controller.rb
@@ -13,5 +13,15 @@ class FaculdadesController < ApplicationController
 
   def index
     @faculdades = Faculdade.where.not("sigla = '-' OR sigla IS NULL").page(params[:page]).per(16)
+
+    if params[:faculdade].present?
+      if params[:faculdade].present? && params[:faculdade].length <= 5
+        @faculdades = Faculdade.where("sigla ILIKE ?", "%#{params[:faculdade]}%").page(params[:page]).per(16)
+      else
+        @faculdades = Faculdade.where("name ILIKE ?", "%#{params[:faculdade]}%").page(params[:page]).per(16)
+      end
+    else
+      @faculdades
+    end
   end
 end
